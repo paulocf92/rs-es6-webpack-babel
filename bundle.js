d2ccd01845e4cf2b045f26da55bac1f8e9db9837
Paulo Filho <pcf.feat@gmail.com>
2019-03-30 11:10:15 -0400

    adding webpack to combine files; webpack delegatesg compilation to babel

--- bundle.js
+++ bundle.js
@@ -1,42 +1,100 @@
-"use strict";
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./main.js");
+/******/ })
+/************************************************************************/
+/******/ ({
 
-var arr = [1, 8, 3, 29, 4, 72, 0, 4, 44];
-var maior = arr.reduce(function (total, next) {
-  return total > next ? total : next;
-}); //console.log(maior);
+/***/ "./main.js":
+/*!*****************!*\
+  !*** ./main.js ***!
+  \*****************/
+/*! no static exports found */
+/***/ (function(module, exports) {
 
-var showMsg = function showMsg(_) {
-  return 'Teste!!!';
-}; //console.log(showMsg());
+eval("var arr = [1, 8, 3, 29, 4, 72, 0, 4, 44];\nvar maior = arr.reduce(function (total, next) {\n  return total > next ? total : next;\n}); //console.log(maior);\n\nvar showMsg = function showMsg(_) {\n  return 'Teste!!!';\n}; //console.log(showMsg());\n\n\nvar usuario = {\n  nome: 'Paulo',\n  idade: 27,\n  endereco: {\n    cidade: 'Dourados',\n    estado: 'MS'\n  }\n}; //console.log(usuario);\n\nvar nome = usuario.nome,\n    idade = usuario.idade,\n    cidade = usuario.endereco.cidade; //console.log(nome, idade, cidade);\n\nfunction mostraDados(_ref) {\n  var nome = _ref.nome,\n      idade = _ref.idade;\n  console.log(nome, idade);\n}\n\nmostraDados(usuario);\nvar arr1 = [1, 2, 7, 23, 22];\nvar newArr = [77, 22].concat(arr1); //console.log(newArr);\n\nvar name = 'Paul';\nvar age = 27;\nvar user = {\n  name: name,\n  age: age,\n  company: 'RocketSeat'\n}; //console.log(user);\n\n//# sourceURL=webpack:///./main.js?");
 
+/***/ })
 
-var usuario = {
-  nome: 'Paulo',
-  idade: 27,
-  endereco: {
-    cidade: 'Dourados',
-    estado: 'MS'
-  }
-}; //console.log(usuario);
-
-var nome = usuario.nome,
-    idade = usuario.idade,
-    cidade = usuario.endereco.cidade; //console.log(nome, idade, cidade);
-
-function mostraDados(_ref) {
-  var nome = _ref.nome,
-      idade = _ref.idade;
-  console.log(nome, idade);
-}
-
-mostraDados(usuario);
-var arr1 = [1, 2, 7, 23, 22];
-var newArr = [77, 22].concat(arr1); //console.log(newArr);
-
-var name = 'Paul';
-var age = 27;
-var user = {
-  name: name,
-  age: age,
-  company: 'RocketSeat'
-}; //console.log(user);
+/******/ });
